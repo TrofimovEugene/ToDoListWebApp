49337c050057584c0c456db12b650b2a0397fa2a
Trofimov Eugene <evgentrofi@gmail.com>
2020-11-06 11:29:07 +0800

    Add Index page.

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListWebApp.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
