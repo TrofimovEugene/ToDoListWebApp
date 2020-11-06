0447fd24de483e51ffed63ccc3bde27e0d47c4f3
Trofimov Eugene <evgentrofi@gmail.com>
2020-11-06 12:17:26 +0800

    User Model.

--- Models/User.cs
+++ Models/User.cs
@@ -1,12 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoListWebApp.Models
 {
+    
     public class User
     {
         [Key]
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
     }
 }
